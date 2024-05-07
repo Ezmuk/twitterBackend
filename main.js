5341936cc76369b754e71dfb6ae621e39e45e43e
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-05-07 11:25:47 +0200

    Ajout de .env
    Ajout de .env

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import express from "express";
 import userRoute from "./routes/user.js";
 import tweetsRoute from "./routes/tweets.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PORT } = process.env;
 
 const app = express();
 
@@ -9,4 +14,4 @@ app.use(express.json());
 app.use("/tweets", tweetsRoute);
 
 app.use("/", userRoute);
-app.listen(4000);
+app.listen(PORT);

2261dae949cd9e0478b9ff03d68f6054a0581af1
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-05-09 08:52:32 +0200

    Ajout du cors( ) dans main
    Ajout du cors( )

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ import express from "express";
 import userRoute from "./routes/user.js";
 import tweetsRoute from "./routes/tweets.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const { PORT } = process.env;
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
 app.use("/tweets", tweetsRoute);
 
 app.use("/", userRoute);
+
 app.listen(PORT);
